b2cc036790677e28a89fe72d182e25a2e2b580c1
DylanRa06 <dylanramones20000@hotmail.com>
2026-07-21 21:31:24 -0500

    Parte 1

--- simulador_completo.js
+++ simulador_completo.js
@@ -1,13 +1,34 @@
+let clientes = [];
+let creditos = [];
 
-  let clientes = [];
-  let creditos = [];
+let tasaInteres = 15;
+let clienteSeleccionado = null;
+let cuotaCalculada = 0;
+let montoCalculado = 0;
+let plazoCalculado = 0;
+let creditoAprobado = false;
 
-  let tasaInteres = 15;
-  let clienteSeleccionado = null;
-  let cuotaCalculada = 0;
-  let montoCalculado = 0;
-  let plazoCalculado = 0;
-  let creditoAprobado = false;
 
-  
-//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+// OCULTAR TODAS LAS SECCIONES
+
+function ocultarSecciones() {
+
+    document.getElementById("parametros").classList.remove("activa");
+    document.getElementById("clientes").classList.remove("activa");
+
+}
+
+
+// MOSTRAR UNA SECCIÓN
+
+function mostrarSeccion(idSeccion) {
+
+    ocultarSecciones();
+
+    document.getElementById(idSeccion).classList.add("activa");
+
+}
+
+
+// Para recuperar o mostrar información usar los métodos
+// de la clase utilitarios.
